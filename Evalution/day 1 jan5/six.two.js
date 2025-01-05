897e39b6f88360db677784931787abf1dd01de82
shagun <shagun0621@gmail.com>
2025-01-05 16:53:52 +0530

    evalution

--- Evalution/day 1 jan5/six.two.js
+++ Evalution/day 1 jan5/six.two.js
@@ -61,20 +61,44 @@
 
 
 
-function analyzeVoterData(voters){
-    return voters.reduce((acc,{age,voted})=>{
-        if(age>=18 && age <=25){
-            acc.numMidsPeople++;
-            if(voted)
-        }
-    })
-},
-{
-    numYoungVotes: /* Number of young voters (aged 18-25) who voted */,
-    numYoungPeople: /* Number of young people (aged 18-25) */,
-    numMidVotesPeople: /* Number of middle-aged voters (aged 26-35) who voted */,
-    numMidsPeople: /* Number of middle-aged people (aged 26-35) */,
-    numOldVotesPeople: /* Number of older voters (aged 36 and above) who voted */,
-    numOldsPeople: /* Number of older people (aged 36 and above) */
+
+function analyzeVoterData(voters) {
+    return voters.reduce((acc, { age, voted }) => {
+      if (age >= 18 && age <= 25) {
+        acc.numYoungPeople++;
+        if (voted) acc.numYoungVotes++;
+      } else if (age >= 26 && age <= 35) {
+        acc.numMidsPeople++;
+        if (voted) acc.numMidVotesPeople++;
+      } else if (age >= 36) {
+        acc.numOldsPeople++;
+        if (voted) acc.numOldVotesPeople++;
+      }
+      return acc;
+    }, {
+      numYoungVotes: 0,
+      numYoungPeople: 0,
+      numMidVotesPeople: 0,
+      numMidsPeople: 0,
+      numOldVotesPeople: 0,
+      numOldsPeople: 0
+    });
   }
+  
+  const voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+  ];
+  
+  console.log(analyzeVoterData(voters));
   
